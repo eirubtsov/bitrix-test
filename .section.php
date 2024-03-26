08deb49da264f03aab0e6da277e53031f28338fd
Evgeniy <eirubtsov@my-email.ru>
2024-03-26 19:15:44 +0300

    Развернут Bitrix

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Тест',
+	'description' => 'Отработка тестового задания',
+	'keywords' => '',
+	'robots' => 'index, follow'
+);
+?>
