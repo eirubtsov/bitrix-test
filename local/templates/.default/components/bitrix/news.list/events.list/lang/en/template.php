8817300bbeab977aab3f26079477c0514c6a6730
Evgeniy <eirubtsov@my-email.ru>
2024-03-27 13:46:39 +0300

    Реализован вывосписка мероприятий через компонент news.list

---
+++ local/templates/.default/components/bitrix/news.list/events.list/lang/en/template.php
@@ -0,0 +1,5 @@
+<?php
+
+$MESS['CT_BNL_ELEMENT_DELETE_CONFIRM'] = 'All the information linked to this record will be deleted. Continue anyway?';
+$MESS['RUBTSOV_NEWS_LIST_EVENTS_LIST_NAME_LIST'] = 'List of participants:';
+$MESS['RUBTSOV_NEWS_LIST_EVENTS_LIST_NAME_LIST_NOT_ELEMENTS'] = 'The participants are not registered yet';
---
+++ local/templates/.default/components/bitrix/news.list/events.list/lang/ru/template.php
@@ -0,0 +1,5 @@
+<?php
+
+$MESS['CT_BNL_ELEMENT_DELETE_CONFIRM'] = 'Будет удалена вся информация, связанная с этой записью. Продолжить?';
+$MESS['RUBTSOV_NEWS_LIST_EVENTS_LIST_NAME_LIST'] = 'Список участников:';
+$MESS['RUBTSOV_NEWS_LIST_EVENTS_LIST_NAME_LIST_NOT_ELEMENTS'] = 'Участники еще не зарегистрированы';
